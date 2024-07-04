9cdb7c514b79413e5d8c674a2cf15b7dd3a9fafd
harshhh05 <harshithamv2305@gmail.com>
2024-07-04 20:24:56 +0530

    Add files via upload

---
+++ assess1.py
@@ -0,0 +1,9 @@
+def check_even_odd(number):
+    if number % 2==0:
+        print(f"{number} is even.")
+    else:
+        print(f"{number} is odd.")
+number =7
+check_even_odd(number)
+number = 10
+check_even_odd(number)
